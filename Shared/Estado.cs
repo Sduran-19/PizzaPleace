8ba8b4e99fe358834cc9bac1222ae751051e9b02
Samuel Durán García <samuel_duran1@ucne.edu.do>
2024-02-04 15:27:33 -0400

    Add project files.

---
+++ Shared/Estado.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace PizzaPlace.Shared;
+
+public class Estado
+{
+	public Menu Menu { get; } = new Menu();
+	public CarritoCompras Carrito { get; } = new CarritoCompras();
+	public UI UI { get; set; } = new UI();
+
+	public decimal PrecioTotal => Carrito.Ordenes.Sum(id => Menu.ObtenerPizza(id)!.Precio);
+}
